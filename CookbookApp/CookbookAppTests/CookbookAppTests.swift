c203964a3d00215e3b1c6da7fa7b08bf7a56a58b
Kyle Baker <kydoggcodes@gmail.com>
2024-11-05 22:20:09 -0500

    Initial project setup

---
+++ CookbookApp/CookbookAppTests/CookbookAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookAppTests.swift
+//  CookbookAppTests
+//
+//  Created by Kyle Baker on 11/5/24.
+//
+
+import Testing
+@testable import CookbookApp
+
+struct CookbookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
